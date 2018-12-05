e88baa05dcdf386f611bb05f4926853c13485af9
Vicrobot <chaosplusorder@gmail.com>
2018-12-05 10:42:03 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from tkinter import *
+from tkinter import font
+from functools import partial
+
+
+img = ['.png','.jpg','.svg','.jpeg','.exif','.tiff','.gif','.bmp','.jfif','.ppm','.pgm','.pbm','.pnm','.webp','.heif','.bat','.bpg']
+vid = ['.mp4','.mkv','.m4a', '.m4v', '.f4v', '.f4a', '.m4b', '.m4r', '.f4b', '.mov','.3gp', '.3gp2', '.3g2', '.3gpp',             '.3gpp2','.ogg','.oga','.ogv','.ogx','.wmv', '.wma', '.asf','.webm','.flv','.ts']
+var = ""
+
+def is_vid(st):
+    for i in vid:
+        if i in st: return True
+    else: return False
+
+def is_img(st):
+    for i in img:
+        if i in st: return True
+    else: return False
+
+def cutnpst(v1, v2, v3):
+    os.rename(v2+'/'+v1,mkname(v3+'/'+v1))
+
+def mkname(name):
+    num = 0
+    woo = name
+    while os.path.exists(woo):
+        num += 0
+        woo = woo + str(num)
+    return woo
+
+def okay(e):
+    global var
+    string = e.get()
+    var = str(string).rstrip()
+
+
+if __name__ == "__main__":
+
+
+    root = Tk()                              # the main object
+    root.title('Filter App')                 # the title
+
+    root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='june.png'))
+    os.chdir('/')
+
+
+    #font
+    helv36 = font.Font(family="Helvetica",size=20,weight="bold")
+    helv37 = font.Font(family="Sans_monospace",size=30,weight="normal")
+
+    #Entry
+    e = Entry(root,bg = '#9ce1e2',bd = 6, font = helv36)  # an Entry object
+    e.pack()                                                             # set and display
+    e.place()
+
+    #Label
+    L = Label(root, text="Give Path",fg = '#013e8e',font = helv37)
+    L.pack( side = 'top')
+    L.place()
+
+    #Button
+    b = Button(root,text='OKAY',command=lambda arg = e: okay(arg), width = 15, height = 2,fg = '#6a7ad1',
+    activeforeground = '#013e8e',font =helv36 ) # a button object
+    b.pack(side = 'bottom')                                                  # set and display
+    b.place()
+
+    root.mainloop()
+
+
+    assert os.path.isdir(var), "Not a directory"
+    path1 = mkname(var+"/videos")
+    path2 = mkname(var+"/images")
+    os.mkdir(path1)
+    os.mkdir(path2)
+    for i in os.listdir(var):
+        if os.path.isfile(var +'/'+ str(i)):
+            if is_vid(i):
+                cutnpst(i,var,path1)
+            if is_img(i):
+                cutnpst(i,var,path2)
+    Label(master = root,text = "Done",).pack()        # label object set and displayed
+
+    sys.exit()
